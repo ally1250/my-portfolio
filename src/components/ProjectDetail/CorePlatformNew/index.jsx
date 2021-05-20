f099e99acc3f58bc3c6253ad3cd0b6acc36cff38
Wang, Danling <daw001@ucsd.edu>
2021-05-19 23:34:55 -0700

    new core platform

--- src/components/ProjectDetail/CorePlatformNew/index.jsx
+++ src/components/ProjectDetail/CorePlatformNew/index.jsx
@@ -71,7 +71,7 @@ const CustomScene = ({
   <Controller>
     <Scene
       triggerHook="onLeave"
-      duration="300%"
+      duration="100%"
       pin
     >
       <Timeline
@@ -83,7 +83,9 @@ const CustomScene = ({
             <div className="description evaluation">
               <div className="label">
                 PRO
-                <ul className="pro">{pro.map((p) => <li>{p}</li>)}</ul>
+                <ul className="pro">
+                  {pro.map((p) => <li>{p}</li>)}
+                </ul>
               </div>
               <div className="label">
                 CON
@@ -133,7 +135,7 @@ class CorePlatform extends Component {
 
   render() {
     return (
-      <div className="amazon-main core-platform-main">
+      <div className="amazon-main core-platform-new-main">
         {/* <Navigation navigations={navigations} /> */}
         <div className="header-container" style={{ backgroundColor: '#bd520a', paddingBottom: '3em' }}>
           <h1 style={{ color: 'white' }}>The CORE Redesign</h1>
@@ -143,7 +145,7 @@ class CorePlatform extends Component {
           </div>
         </div>
 
-        <div className="header-container sections" style={{ boxShadow: 'none', paddingBottom: 0, paddingTop: '2em' }}>
+        <div className="header-container sections" style={{ boxShadow: 'none', paddingBottom: 0, paddingTop: '4em' }}>
           <p>
             <div className="section-header">Links</div>
             <a href={headerSections.links[1].url} target="_blank" rel="noreferrer">{headerSections.links[1].label}</a>
@@ -174,20 +176,47 @@ class CorePlatform extends Component {
         </div>
 
         <div className="design-process content-container">
-          <AnimatedHeader style={{ textTransform: 'uppercase' }}>Problem Statement</AnimatedHeader>
-          <p>Due to disjointed communities, health researchers need a way to effectively connect with other people in the same field so they can easily gather information and share experiences among themselves in order to facilitate ethical health research.</p>
+          <AnimatedHeader style={{ textTransform: 'uppercase', margin: 0, padding: 0 }}>Overview</AnimatedHeader>
+          <p>
+            In order to overcome this common issue faced by the CORE Platform user, we decided to redesign the CORE Platform to improve the functionality of various features of the platform. The project consists of four stages:
+            <ol>
+              <li>user research</li>
+              <li>prototyping</li>
+              <li>user testing</li>
+              <li>implementation</li>
+            </ol>
+            Each stage requires my knowledge in both design and coding in order to find a balance between what is wanted and what is feasible. After the new design was finalized by the whole team, I started implementing the front-end in React.js with my partner working on the back-end with Node.js.
+          </p>
+        </div>
+
+        <div className="design-process content-container problem-statement">
+          <AnimatedHeader style={{ textTransform: 'uppercase', margin: 0 }}>Problem Statement</AnimatedHeader>
+          Due to disjointed communities, health researchers need a way to effectively connect with other people in the same field so they can easily gather information and share experiences among themselves in order to facilitate ethical health research.
         </div>
 
         <div className="design-process content-container">
-          <AnimatedHeader style={{ textTransform: 'uppercase' }}>Project Overview</AnimatedHeader>
+          <AnimatedHeader style={{ textTransform: 'uppercase', margin: 0, padding: 0 }}>User Research</AnimatedHeader>
+          <p>
+            We interviewed several members on the team and some current users of the CORE Platform. From our conversations, I summarized two main issues the current design has that caused users to feel disconnected with each other:
+            <ul>
+              <li>
+                <b>Lack of ways to contact and communicate with each other.</b>
+                {' '}
+                Even though the current platform has a Network page for users to view others information, it does not have the functionality that supports communication between users. We also noticed that only a few users provided their contact information in their profiles, which makes us feel the importance of encouraging users to be more involved.
+              </li>
+              <li>
+                <b>Lack of ways to express thoughts and share expertise.</b>
+                {' '}
+                The CORE Platform has a Forum where users can ask questions and respond to each other’s posts. However, the interactivity for the Forum is low so in order to improve this, we need simpler ways for users to participate in Forum discussions.
+              </li>
+            </ul>
+          </p>
         </div>
 
         <div className="design-process content-container">
-          <AnimatedHeader style={{ textTransform: 'uppercase' }}>User Research</AnimatedHeader>
-        </div>
-
-        <div className="design-process content-container">
-          <AnimatedHeader style={{ textTransform: 'uppercase' }}>Prototyping</AnimatedHeader>
+          <AnimatedHeader style={{ textTransform: 'uppercase', margin: 0, padding: 0 }}>Prototyping</AnimatedHeader>
+          <h3>The Network Page</h3>
+          <p>I added a chat room functionality for users to communicate within the platform. This eliminates the trouble of finding someone’s contact information and messaging them through another means. It makes communication more accessible and efficient for health researchers and would encourage them to share ideas and learn from other experts in the field.</p>
         </div>
 
         <div className="indent-container indent-image">
@@ -195,10 +224,20 @@ class CorePlatform extends Component {
         </div>
         <div className="caption small-indent">Chat Room Design</div>
 
+        <div className="design-process content-container">
+          <h3>The Forum Page</h3>
+          <p>I included a new voting functionality in which users can up-vote or down-vote all Forum posts. Users can now quickly and easily express their opinion on the discussion they are interested in by giving their votes. The votes also act as a criteria for users to refer to when considering the usefulness of the discussions.</p>
+        </div>
+
         <div className="indent-container indent-image">
           <img src={COREForum} alt="amazon project sketch 1" style={{ width: 850 }} />
         </div>
         <div className="caption small-indent">Up-vote and Down-vote Feature</div>
+
+        {/* <div className="design-process content-container">
+          <AnimatedHeader style={{ textTransform: 'uppercase' }}>User Testing</AnimatedHeader>
+          <p>I presented my design to the team and asked a few participants to walk through the prototype and give their feedback. With the user testing results, I iterated on the redesign to create the final version.</p>
+        </div> */}
 
         <div className="divider" />
 
@@ -255,11 +294,8 @@ class CorePlatform extends Component {
         </CustomScene>
 
         <div className="design-process content-container">
-          <AnimatedHeader style={{ textTransform: 'uppercase' }}>User Testing</AnimatedHeader>
-        </div>
-
-        <div className="design-process content-container">
-          <AnimatedHeader style={{ textTransform: 'uppercase' }}>Development</AnimatedHeader>
+          <AnimatedHeader style={{ textTransform: 'uppercase', marginTop: '3em', padding: 0 }}>Development</AnimatedHeader>
+          <p>To help keep track of progress, we created a detailed week-to-week plan before starting on coding. We used React with SCSS for building the front-end for more customizability in the UI. For the back-end, we used GraphQL to manage and retrieve data from the database. </p>
         </div>
 
         <div className="indent-container indent-image">
@@ -268,8 +304,18 @@ class CorePlatform extends Component {
         <div className="caption">Screenshot of Week 2 Development Plans</div>
 
         <div className="design-process content-container">
-          <AnimatedHeader style={{ textTransform: 'uppercase' }}>Reflection</AnimatedHeader>
-          <p>To be added...</p>
+          <AnimatedHeader style={{ textTransform: 'uppercase', marginTop: '3em', padding: 0 }}>Reflection</AnimatedHeader>
+          <p>This project was a valuable experience for me as it allowed me to gain knowledge on the entire workflow of building an application from scratch. </p>
+          <p>One thing I wish I have done better is to come up with a concrete timeline that is actually feasible. The initial week-to-week plan was on an extremely tight schedule but we failed to notice it until we were in the middle of the plan. Each week we tried our best to follow the plan but still needed to move some work to the following week. The work gradually built up so we had to push our deadline back multiple times.</p>
+          <p>
+            If I were to do a similar project in the future, I would create a less stressful plan and give more accurate estimations for each task. I remember a professor once said,
+            <i><b>“the actual time needed to complete any task equals your own estimate times two plus 1”</b></i>
+            . One important lesson I learned from this project is that do not ever underestimate the workload and try not to overestimate your speed.
+            It is better to set a late deadline than to keep pushing back the deadline because the later would cause the client to question your reliability.
+          </p>
+
+          <div className="divider" />
+
           <p>
             <div className="feedback-section">
               As both a designer and developer during the redesign of process, Ally has created and developed designs that have dramatically changed the CORE platform for the betterment of our users. Each section and feature of the platform were carefully evaluated and improved in close collaboration with the rest of the team, and Ally consistently gave quality and constructive inputs throughout. She is also persistent, recreating, and iterating whenever she felt something could be improved in the prototyping process, creating a noticeable polish in the final design. I am lucky to have worked with her on this project and wish her well on future ones.
